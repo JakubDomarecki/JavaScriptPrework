fecfcd2e763c528061b5ceee40662dc7089f9d2c
kuba86952@wp.pl <kuba86952@wp.pl>
2024-02-25 18:52:16 +0100

    Zmienne-solved

---
+++ 02_Typy_danych/02_Zmienne/js/app.js
@@ -0,0 +1,20 @@
+// Ta zmienna jest typu number i przechowuje wartość 5
+const numberValue = 5;
+console.log(numberValue);
+console.log(typeof numberValue);
+// Ta zmienna jest typu string i przechowuje wartość 'Ala ma kota'
+const stringValue = 'Ala ma kota';
+console.log(stringValue);
+console.log(typeof stringValue);
+// Ta zmienna jest typu string i przechowuje wartość  ${2}two
+const mixedValue = '${2}two';
+console.log(mixedValue);
+console.log(typeof mixedValue);
+// Ta zmienna jest typu boolean i przechowuje wartość  true
+const logicValue = true;
+console.log(logicValue);
+console.log(typeof logicValue);
+// Ta zmienna jest typu  null i przechowuje wartość  null
+const nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue);
