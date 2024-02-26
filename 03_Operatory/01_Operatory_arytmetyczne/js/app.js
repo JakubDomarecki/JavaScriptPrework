d9f98f25b085f40be2999011556f83db6384cc10
kuba86952@wp.pl <kuba86952@wp.pl>
2024-02-26 15:12:51 +0100

    Operatory_arytmetyczne-solved

---
+++ 03_Operatory/01_Operatory_arytmetyczne/js/app.js
@@ -0,0 +1,14 @@
+const johnAge = 25;
+const aliceAge = 30;
+const bobAge = 17;
+
+const ageSum = johnAge + aliceAge;
+const ageModulo = aliceAge % johnAge;
+const ageDivide = aliceAge / bobAge;
+
+console.log(ageSum);
+console.log(ageModulo);
+console.log(ageDivide);
+
+const ageOperations = ageSum * ageModulo;
+console.log(ageOperations);
