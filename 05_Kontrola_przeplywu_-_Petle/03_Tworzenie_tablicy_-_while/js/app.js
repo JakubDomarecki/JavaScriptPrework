105a2f2f4f4099382ecde54ab437e2fc666ad332
kuba86952@wp.pl <kuba86952@wp.pl>
2024-02-27 15:10:42 +0100

    Tworzenie_tablicy_-_while-solved

--- 05_Kontrola_przeplywu_-_Petle/03_Tworzenie_tablicy_-_while/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/03_Tworzenie_tablicy_-_while/js/app.js
@@ -3,3 +3,12 @@ const numbers = [];
 /**
  * Write your code below!
  */
+let i = 0;
+let n = 100;
+while (i <= n) {
+	i++;
+	if (i % 3 === 0 && i % 5 === 0) {
+		numbers.push(i);
+	}
+}
+console.log(numbers);
