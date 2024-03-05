bd1fc64e8f30bab8db71f8bf27215e8de6dd67a8
kuba86952@wp.pl <kuba86952@wp.pl>
2024-03-05 13:50:28 +0100

    tabliczka_mnozenia-solved

--- 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
@@ -4,3 +4,13 @@ const calc = [];
 /**
  * Write your code below!
  */
+for(let i = 1; i <= n; i++) {
+	const row = [];
+	for (let j = 1; j <= n; j++) {
+		row.push(`${i} x ${j} = ${i * j}`);
+	}
+	calc.push(row);
+}
+for(const row of calc) {
+	console.log(row.join(' | '));
+}
