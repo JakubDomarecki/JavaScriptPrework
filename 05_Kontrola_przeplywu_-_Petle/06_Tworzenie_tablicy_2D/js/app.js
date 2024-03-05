64fa41d356a29551574416e361e13107351ffeb3
kuba86952@wp.pl <kuba86952@wp.pl>
2024-03-05 13:01:20 +0100

    tworzeenie_tablicy_2D-solved

--- 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
@@ -5,3 +5,14 @@ const rows = 5;
 /**
  * Write your code below!
  */
+let count = 1;
+for (let i = 0; i < rows; i++) {
+	const row = [];
+	for (let j = 0; j < columns; j++) {
+		row.push(count);
+		count++;
+	}
+	numbers.push(row);
+}
+console.log(numbers);
+
