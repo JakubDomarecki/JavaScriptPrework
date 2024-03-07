573bc8c701ba5b46848d53462b4ebd1ea25a2aca
kuba86952@wp.pl <kuba86952@wp.pl>
2024-03-07 15:27:51 +0100

    wyswietlanie_tablicy_2D_funkcje-solved

--- 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
+++ 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
@@ -6,7 +6,14 @@ const users = [
 /**
  * Write your code below!
  */
-
+function print2DArray(array) {
+  for (let i = 0; i < array.length; i++){
+    for (let j = 0; j < array[i].length; j++){
+      console.log(array[i][j]);
+    }
+  }
+}
+print2DArray(users);
 /**
  * Do NOT modify code below!
  */
