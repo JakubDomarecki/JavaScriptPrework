6b8f43df16a4a4e025885177b1419620a3b93b7b
kuba86952@wp.pl <kuba86952@wp.pl>
2024-03-07 19:23:59 +0100

    wartosc_maksymalna_funkcje-solved

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -3,3 +3,8 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 /**
  * Write your code below!
  */
+function maxFromArray(numbers) {
+		return Math.max(...numbers);
+}
+const result = maxFromArray(randomNumbers);
+console.log(result);
